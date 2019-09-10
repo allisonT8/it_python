211ca68485e54103371a6f403a3200e34dfd33c8
Allison Truskowski <allisontrus8@gmail.com>
2019-09-10 19:43:19 -0400

    Initial Commit

---
+++ guess.py
@@ -0,0 +1,31 @@
+# code go's here
+import random
+
+print("==============================")
+print("     GUESS MY NUMBER          ")
+print("      by Allison.T            ")
+print("==============================")
+print("")
+
+name = input("What is your name?")
+print("")
+
+print("I'm thinking of a number between 0 and 100. Can you guess it?")
+the_number = random.randint(1,99)
+
+guess = -1
+
+while guess != the_number:
+    guess_text = input("What is you guess?")
+    guess = int(guess_text)
+
+    if guess > 100 or guess < 0:
+        print("Please choose a number that is between 0 and 100. :-|")
+    elif guess < the_number:
+        print(f"Sorry {name}, but your guess is too LOW. Try again. :-(")
+    elif guess > the_number:
+        print(f"Sorry {name}, but your guess is too HIGH. Try again. :-(")
+    else:
+        print(f"You guessed it! Congratulations, {name}! :-)")
+
+print("Thanks for playing! :-)")
