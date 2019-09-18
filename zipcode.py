884ff410a979e86093a66906e1867cdae05e1b77
Allison Truskowski <allisontrus8@gmail.com>
2019-09-18 09:58:38 -0400

    Zip code 1 hr

---
+++ zipcode.py
@@ -0,0 +1,12 @@
+from banner import banner
+import random
+#1. get zipcode working
+#2. have is reset
+#3. then not a zipcode in the county happend
+banner("Zip Code Sorter","Allison.T")
+
+zipcode = input("Please enter a zipcode: ")
+county =
+awsers = input(f"The zipcode {zipcode} is for {county}")
+
+retry = input("Would you like to enter another zipcode (Y/N)?: ")
