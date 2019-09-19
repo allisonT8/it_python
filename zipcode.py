00938318472d60e90f47058d4c1f20c7ccc1928c
Allison Truskowski <allisontrus8@gmail.com>
2019-09-19 10:35:14 -0400

    updata

--- zipcode.py
+++ zipcode.py
@@ -1,12 +1,30 @@
 from banner import banner
-import random
-#1. get zipcode working
-#2. have is reset
-#3. then not a zipcode in the county happend
 banner("Zip Code Sorter","Allison.T")
 
-zipcode = input("Please enter a zipcode: ")
-county =
-awsers = input(f"The zipcode {zipcode} is for {county}")
+print("Welcome to the Newaygo County zipcode sorter OwO .")
 
-retry = input("Would you like to enter another zipcode (Y/N)?: ")
+while true:
+    zip_code = int(input("Please enter a zipcode: "))
+    if zip_code == 49309:
+        print(f"The zipcode {zip_code} is for Bitely.")
+    elif zip_code == 49312:
+        print(f"The zipcode {zip_code} is for Brohman.")
+    elif zip_code == 49337:
+        print(f"The zipcode {zip_code} is for both Newaygo and Croton.")
+    elif zipcode == 49412:
+        print(f"The zipcode {zip_code} is for Fremont")
+    elif zipcode == 49412:
+        print(f"The zipcode {zip_code} is for Fremont")
+    elif zipcode == 49327:
+        print(f"The zipcode {zip_code} is for Grant")
+    elif zipcode == 49349:
+        print(f"The zipcode {zip_code} is for White Cloud")
+    else:
+        print(f"The zipcode {zip_code} is not the in Newaygo County")
+
+    if input("Would you like to enter another zipcode (Y/N)") == "Y":
+        continue
+    else:
+        break
+
+print("Thanks for using me! UwU")
